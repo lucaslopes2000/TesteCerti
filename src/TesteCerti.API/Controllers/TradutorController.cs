22e79525d77c199de676999e92906e0f122509d8
lucaslopes2000 <lucassl2011@gmail.com>
2022-02-13 14:30:43 -0300

    first commit

---
+++ src/TesteCerti.API/Controllers/TradutorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TesteCerti.API.Models;
+
+namespace TesteCerti.API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class TradutorController : ControllerBase
+    {
+        public TradutorController()
+        {
+            
+        }
+
+        [HttpGet("{number}")]
+        public Tradutor Get(int number)
+        {
+            var name = Convert.ToString(number);
+
+            return new Tradutor() {
+                Extenso = name
+            };
+        }
+    }
+}
---
+++ src/TesteCerti.API/Models/Tradutor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteCerti.API.Models
+{
+    public class Tradutor
+    {
+        public string Extenso { get; set; }
+    }
+}
